645056e512601e725e7829019a3fb893f1d92fc9
ayahabedellatif-1 <ayahabedellatif@gmail.com>
2026-07-22 14:14:35 +0300

    enviroment1.1

---
+++ test1.py
@@ -0,0 +1,16 @@
+import cv2
+import numpy as np 
+cap = cv2.VideoCapture(0)
+while(1):
+    ret,Frame=cap.read()
+    if not ret:
+         break
+    cv2.imshow("original",Frame)
+    if cv2.waitKey(1)==ord('q'):
+         break
+cap.release()
+cv2.destroyAllWindows()
+
+ 
+ 
+ 
